ae41bd7ec78dc81ccd02a4aafd3c4b2e861db760
Praveen De Silva <desilvabkp.23@uom.lk>
2025-02-10 23:54:50 +0530

    p3174

---
+++ p3174_ClearDigits.cpp
@@ -0,0 +1,18 @@
+#include <cctype>
+
+class Solution {
+public:
+    string clearDigits(string s) {
+        int i = 0;
+
+        while (i < s.size()) {
+            if (i > 0 && isdigit(s[i])) {
+                i--;
+                s.erase(i,2);
+                continue;
+            }
+            i++;
+        }
+        return s;
+    }
+};
