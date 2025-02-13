73dd502227820e18ae5e907e57fa0acfe62cfd69
Praveen De Silva <desilvabkp.23@uom.lk>
2025-02-13 21:20:53 +0530

    Power of two

---
+++ p231_PowerOfTwo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+    public:
+        bool isPowerOfTwo(int n) {
+            if (n==1) {
+                return true;
+            } else if (n>1 && n%2==0) {
+                return isPowerOfTwo(n/2);
+            } else {
+                return false;
+            }
+        }
+    };
