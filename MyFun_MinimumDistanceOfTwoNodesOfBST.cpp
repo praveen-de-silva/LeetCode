478721359a81a916b58d530acc0dec3b8c0a8194
Praveen De Silva <desilvabkp.23@uom.lk>
2025-06-24 06:51:20 +0530

    Add implementation for finding the minimum distance between two nodes in a binary search tree

---
+++ MyFun_MinimumDistanceOfTwoNodesOfBST.cpp
@@ -0,0 +1,34 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+using namespace std;
+
+int findDist(int u, int v) {
+    int crnt_u = u, crnt_v = v;
+    int dist_u = 0, dist_v = 0;
+    
+    while (true) {
+        while (crnt_u > crnt_v) {
+            crnt_u = (crnt_u-1) / 2;
+            dist_u++;
+        }
+        
+        while (crnt_v > crnt_u) {
+            crnt_v = (crnt_v-1) / 2;
+            dist_v++;
+        }
+        
+        if (crnt_u == crnt_v) {
+            break;
+        }
+    }
+    
+    return dist_u + dist_v;
+}
+
+int main() {
+    
+    cout << findDist(8, 2);
+
+    return 0;
+}
