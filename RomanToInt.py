5b9825ea090a148f7d56c1637b22f884923b9831
Praveen De Silva <desilvabkp.23@uom.lk>
2025-12-03 23:48:41 +0530

    Uncomment romanToInt method implementation
    Uncomment the romanToInt method and its logic for processing Roman numerals.

--- RomanToInt.py
+++ RomanToInt.py
@@ -10,13 +10,13 @@ class Solution(object):
     #     'M':1000
     #     }
 
-    # def romanToInt(self, s):
-    #     if len(s)==1:
-    #         return self.data[s]
+    def romanToInt(self, s):
+        if len(s)==1:
+            return self.data[s]
 
-    #     if self.romanToInt(s[0])>= self.romanToInt(s[1]):
-    #         return self.romanToInt(s[0]) + self.romanToInt(s[1:])
-    #     return - self.romanToInt(s[0]) + self.romanToInt(s[1:])
+        if self.romanToInt(s[0])>= self.romanToInt(s[1]):
+            return self.romanToInt(s[0]) + self.romanToInt(s[1:])
+        return - self.romanToInt(s[0]) + self.romanToInt(s[1:])
 
     # Method 02:
 
