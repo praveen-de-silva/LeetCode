b659f03ab597732e9feaa95ea88d6aabf46b6b4d
Praveen De Silva <desilvabkp.23@uom.lk>
2025-12-09 11:22:25 +0530

    Efficient way of finding center of star graph
    Refactor to simplify finding the center of the star graph by using the first edge as a guess and checking for nodes not in the edge.

--- p1791_FindCenterOfStarGraph.py
+++ p1791_FindCenterOfStarGraph.py
@@ -4,34 +4,14 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: int
         """
-        graph = dict()
-        center = int()
-        i = 0 # to prevent long run
+        guess = edges[0]
 
-        # --------------
-        # make the graph
-        # --------------
-
-        for u,v in edges:
-            if i > 10:
+        for edge in edges:
+            if len(guess) == 1:
                 break
-
-            if v in graph.get(u, []): # check whether u-v exisits (avoid duplicate edges)
-                continue    
             
-            graph[u] = graph.get(u, []) + [v]
-            graph[v] = graph.get(v, []) + [u]
-            
-            i += 1
-        
-        # ---------------------
-        # check the center node
-        # ---------------------
-
-        size = len(graph.keys()) # size of the graph
-
-        for u, neighbours in graph.items():
-            if len(neighbours) == size - 1:
-                center = u
+            for node in guess:
+                if node not in edge:
+                    guess.remove(node)
                 
-        return center
+        return guess[0]
