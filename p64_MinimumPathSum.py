1ee939341cb2aa31ac31ce9d39a1a5ebce501f32
Praveen De Silva <desilvabkp.23@uom.lk>
2025-12-12 18:34:48 +0530

    Update p64_MinimumPathSum.py

--- p64_MinimumPathSum.py
+++ p64_MinimumPathSum.py
@@ -1,3 +1,7 @@
+# -------------------------
+# Method 01 : With DP table
+# -------------------------
+
 class Solution(object):
     def minPathSum(self, grid):
         """
@@ -28,3 +32,37 @@ class Solution(object):
                 dpGrid[dx][dy] = crntVal
   
         return dpGrid[rows-1][cols-1]
+
+# ----------------------------------------------------
+# Method 02 : With DP table but no external space used
+# ----------------------------------------------------
+
+class Solution(object):
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        rows, cols = len(grid), len(grid[0]) # no of rows and cols
+
+        for dx in range(rows):
+            for dy in range(cols):
+                
+                crntVal = grid[dx][dy]
+
+                if dx == 0 and dy == 0:
+                    pass
+                elif dx == 0 and dy > 0:
+                    crntVal += grid[dx][dy-1]
+                elif dx > 0 and dy == 0:
+                    crntVal += grid[dx-1][dy]
+                else:
+                    if grid[dx][dy-1] < grid[dx-1][dy]: # same issue
+                        crntVal += grid[dx][dy-1]
+                    else:
+                        crntVal += grid[dx-1][dy]
+
+                grid[dx][dy] = crntVal
+  
+        return grid[rows-1][cols-1]
+
