a20443057556a5dc3959112e957ae815fb3b5031
Praveen De Silva <desilvabkp.23@uom.lk>
2025-12-24 00:15:17 +0530

    Correct event overlap checks in maxTwoEvents
    Fix logic in maxTwoEvents method to correctly check event overlaps and calculate maximum values.

--- p2054_TwoBestNonOverlappingEvents.py
+++ p2054_TwoBestNonOverlappingEvents.py
@@ -1,26 +1,29 @@
+# ---------
+# Method 01
+# ---------
+# Time compexity = O(n^2)
+
 class Solution:
     def maxTwoEvents(self, events: List[List[int]]) -> int:
         n = len(events)
         events.sort()
-        print(events)
+        
         vals = [0] * n
 
         
 
         for i in range(n):
             j = i+1
-            maxVal
-            
-            while j < n and events[j][0] <= events[i][0]:
+            while j < n and events[j][0] <= events[i][1]:
                 j += 1
 
-            if j == n:
-                continue
-
-            
-
-            for k in range(j, n):
-
-        
-        return 4
+            maxVal = 0
+            if j < n:
+                for k in range(j, n):
+                    maxVal = max(maxVal, events[k][2])
+            maxVal += events[i][2]
+            vals[i] = maxVal
+        # print(events)
+        # print(vals)
+        return max(vals)
         
