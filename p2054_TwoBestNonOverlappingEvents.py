33c5911f61296c48c51d0d9dea43b5095b70c19f
Praveen De Silva <desilvabkp.23@uom.lk>
2025-12-24 22:49:40 +0530

    Add method to calculate max two non-overlapping events
    Implemented a solution to find the maximum value of two non-overlapping events using binary search and dynamic programming.

--- p2054_TwoBestNonOverlappingEvents.py
+++ p2054_TwoBestNonOverlappingEvents.py
@@ -26,4 +26,33 @@ class Solution:
         # print(events)
         # print(vals)
         return max(vals)
+
+# ---------
+# Method 02
+# ---------
+# Time complexity = O(n*lg(n))
+
+from bisect import bisect_right
+
+class Solution:
+    def maxTwoEvents(self, events: list[list[int]]) -> int:
+        events.sort()  # Sort by start time
+        n = len(events)
+        starts = [e[0] for e in events]  # For binary search
+        maxFromHere = [0] * n  # max value from i to end
         
+        # --- fill maxFromHere from the end ---
+        maxFromHere[-1] = events[-1][2]
+        for i in range(n-2, -1, -1):
+            maxFromHere[i] = max(events[i][2], maxFromHere[i+1])
+        
+        # --- sequentally find the maximum value ---
+        result = 0
+        for i in range(n):
+            j = bisect_right(starts, events[i][1]) # index after all values <= events[i][1]
+            total = events[i][2] # current value
+            if j < n:
+                total += maxFromHere[j] # max value in the right side
+            result = max(result, total)
+        
+        return result
