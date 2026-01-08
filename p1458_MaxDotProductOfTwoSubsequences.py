08ec68aae843b4c413615510d71b4abcf1b0be1a
Praveen De Silva <desilvabkp.23@uom.lk>
2026-01-09 00:59:00 +0530

    Add maxDotProduct method to Solution class
    Implement dynamic programming solution for maximum dot product of two subsequences.

---
+++ p1458_MaxDotProductOfTwoSubsequences.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
+        n, m = len(nums1), len(nums2)
+        dp = [[float('-inf') ] * m for _ in range(n)]
+
+        for i in range(n):
+            for j in range(m):
+                crntMul = nums1[i] * nums2[j]
+                dp[i][j] = crntMul
+
+                if i>0 and j>0:
+                    dp[i][j] = max(crntMul, crntMul + dp[i-1][j-1])
+                if i>0:
+                    dp[i][j] = max(dp[i][j], dp[i-1][j])
+                if j>0:
+                    dp[i][j] = max(dp[i][j], dp[i][j-1])
+ 
+        return dp[n-1][m-1]
+         
+
+            
+        
